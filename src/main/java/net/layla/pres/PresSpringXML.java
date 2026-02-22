3de3458ecc885cdfabce941f0c389704c880dd12
layla <laylaelhajjaji@gmail.com>
2026-02-22 23:25:22 +0000

    Instanciation Framework Spring  Version XML

---
+++ src/main/java/net/layla/pres/PresSpringXML.java
@@ -0,0 +1,13 @@
+package net.layla.pres;
+
+import net.layla.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PresSpringXML {
+    public static void main(String[] args) {
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier = (IMetier) springContext.getBean("metier");
+        System.out.println("RES="+metier.calcul());
+    }
+}
