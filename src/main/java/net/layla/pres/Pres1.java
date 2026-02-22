afdbc0ce062d12edf13fd4052f55349b6acda702
layla <laylaelhajjaji@gmail.com>
2026-02-22 22:15:00 +0000

    Résolution du NullPointerException dans Pres1 main()

--- src/main/java/net/layla/pres/Pres1.java
+++ src/main/java/net/layla/pres/Pres1.java
@@ -1,10 +1,13 @@
 package net.layla.pres;
 
+import net.layla.dao.DaoImpl;
 import net.layla.metier.MetierImpl;
 
 public class Pres1 {
     public static void main(String[] args) {
+        DaoImpl d = new DaoImpl();
         MetierImpl metier = new MetierImpl();
+        metier.setDao(d);
         System.out.println("RES = "+metier.calcul());
     }
 }
