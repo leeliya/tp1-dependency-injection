0dc596ba5f5d06d02735dbd7ed08819c5ba3c479
layla <laylaelhajjaji@gmail.com>
2026-02-22 22:47:42 +0000

    Instanciation statique de DaoImplV2

---
+++ src/main/java/net/layla/net/layla/ext/DaoImplV2.java
@@ -0,0 +1,15 @@
+package net.layla.net.layla.ext;
+
+import net.layla.dao.IDao;
+
+public class DaoImplV2 implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("Version capteurs ...");
+        double t = 12;
+        return t;
+    }
+}
+
+
--- src/main/java/net/layla/pres/Pres1.java
+++ src/main/java/net/layla/pres/Pres1.java
@@ -2,10 +2,11 @@ package net.layla.pres;
 
 import net.layla.dao.DaoImpl;
 import net.layla.metier.MetierImpl;
+import net.layla.net.layla.ext.DaoImplV2;
 
 public class Pres1 {
     public static void main(String[] args) {
-        DaoImpl d = new DaoImpl();
+        DaoImplV2 d = new DaoImplV2();
         MetierImpl metier = new MetierImpl(d);
         //metier.setDao(d);
         System.out.println("RES = "+metier.calcul());
